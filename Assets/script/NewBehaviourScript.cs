e1f4b020f354d0b322fbbdae83edbe7dac4e6bb3
DESKTOP-O2TPLLH\wa260 <wagudo wa2600871592@gmail.com>
2021-11-15 12:13:47 +0900

    no message

---
+++ Assets/script/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField, Tooltip("移動スピード")]
+    private int moveSpeed;
+
+    [SerializeField]
+    private Animator playerAnim;
+
+    public Rigidbody2D Rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical") * moveSpeed);
+    }
+}
