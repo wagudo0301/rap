7029f18831d11504c55073effef3e45e36ada631
HidekazuTERASOMA <g020c1169@g.neec.ac.jp>
2022-01-10 20:58:45 +0900

    NPCの動き方のスクリプト追加

--- Assets/script/Map/NonPlayerCharacter.cs
+++ Assets/script/Map/NonPlayerCharacter.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class NonPlayerCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<(int,int,float)> MoveCommandList=new List<(int,int,float)>();
+
+    [SerializeField]
+    private int moveSpeed;
+
+    [SerializeField]
+    private Animator playerAnim;
+
+    public Rigidbody2D Rb;
+    
+    private (int,int,float) MoveCommand=(0,0,0f);
+    private float Timer=0f;
+
     void Start()
     {
-        
+        playerAnim.speed=0;
+
+        MoveCommandList.Add((1,0,3));
+        MoveCommandList.Add((-1,0,4));
+        MoveCommandList.Add((0,1,1));
+        MoveCommandList.Add((0,-1,1));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(MoveCommandList.Count!=0&&MoveCommand==(0,0,0f))
+        {
+            Debug.Log(""+MoveCommandList[0].Item1+MoveCommandList[0].Item2+MoveCommandList[0].Item3);
+            MoveCommand=MoveCommandList[0];
+            MoveCommandList.RemoveAt(0);
+
+            playerAnim.SetFloat("X", MoveCommand.Item1);
+            playerAnim.SetFloat("Y", MoveCommand.Item2);
+            if(MoveCommand.Item1==0&&MoveCommand.Item2==0)
+            {
+                playerAnim.speed=0;
+            }
+            else
+            {
+                playerAnim.speed=1;
+                Rb.velocity = new Vector2(MoveCommand.Item1,MoveCommand.Item2).normalized * moveSpeed;
+            }
+        }
+        //MoveCommandあれば
+        if(MoveCommand!=(0,0,0f))
+        {
+            Timer+=Time.deltaTime;
+            if(Timer>=MoveCommand.Item3)
+            {
+                playerAnim.speed=0;
+                Rb.velocity = new Vector2(0,0);
+                MoveCommand=(0,0,0);
+                Timer=0f;
+            }
+        }
     }
 }
