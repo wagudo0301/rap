411dc5922522de40b5fd8f57fc19a0493d268b77
G020C1174-PC_g020c1174 <g020c1174@g.neec.ac.jp>
2022-02-09 23:11:19 +0900

    永続やめるボタンーバグあり

--- Assets/Project/TitleScenes/EizokuQuit.cs
+++ Assets/Project/TitleScenes/EizokuQuit.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuitButtonScripts : MonoBehaviour
+public class EizokuQuit : MonoBehaviour
 {
-    public void ButtonExit()
-    {
-        Application.Quit();
-    }
-    
     // Start is called before the first frame update
     void Start()
     {
-       
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
