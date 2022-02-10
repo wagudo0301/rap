ac185b311b8e892528011bafe42a20f36575a6a7
G020C1174-PC_g020c1174 <g020c1174@g.neec.ac.jp>
2022-02-10 15:39:49 +0900

    ・buildsetting変えたからstaffrollシーンは大丈夫 ・SE追加 ・staffrollで五秒後に3回押したらスキップできる

---
+++ Assets/Project/StaffRoll/sozai/staffroll_cr.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class staffroll_cr : MonoBehaviour
+{   
+    float time = 0f;
+    int cnt = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;//毎フレームの時間を加算.
+        
+        if (Input.GetMouseButtonDown(0) && time >= 5f){
+            cnt += 3;
+            Debug.Log(cnt+"だよ");
+        }
+        
+        
+        if (cnt == 3)
+        {
+           SceneManager.LoadScene("TitleScenes");
+        }
+        
+    }
+}
